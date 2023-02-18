1fb561363e4edfc2bcfcadbc25c592155b8b1c55
Anastasiya S <nastya@status.im>
2023-02-18 21:51:49 +0300

    tests for contacts are now using app fixture

--- application.py
+++ application.py
@@ -8,10 +8,35 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
+    # general helper methods
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
     def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("https://localhost/addressbook/index.php")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def destroy(self):
+        self.wd.quit()
+
+    # group tests helper methods
     def return_to_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
@@ -39,28 +64,25 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, username, password):
+    # contact tests helper methods
+    def return_to_home_page_from_confirmation(self, wd):
         wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_link_text("home page").click()
 
-    def open_home_page(self):
+    def add_contact_without_group(self, contact):
         wd = self.wd
-        wd.get("https://localhost/addressbook/index.php")
+        self.init_contact_creation_home_page()
+        # add a contact without group (none value) from home page
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.contact_first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.contact_last_name)
+        wd.find_element_by_xpath("//option[@value='[none]']").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page_from_confirmation(wd)
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-
-    def destroy(self):
-        self.wd.quit()
-
-
+    def init_contact_creation_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,73 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
-
+import pytest
+from application import Application
 from contact import Contact
 
 
-def is_alert_present(self):
-    try:
-        self.wd.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_contact_without_group(self):
-        self.login(username="admin", password="secret")
-        self.add_contact_without_group(Contact(contact_first_name="First name", contact_last_name="Last name"))
-        self.logout()
-
-    def return_to_home_page_from_confirmation(self, wd):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def add_contact_without_group(self, contact):
-        wd = self.wd
-        self.init_contact_creation_home_page(wd)
-        # add a contact without group (none value) from home page
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.contact_first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.contact_last_name)
-        wd.find_element_by_xpath("//option[@value='[none]']").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page_from_confirmation(wd)
-
-    def init_contact_creation_home_page(self, wd):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("https://localhost/addressbook/group.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact_without_group(app):
+    app.login(username="admin", password="secret")
+    app.add_contact_without_group(Contact(contact_first_name="First name", contact_last_name="Last name"))
+    app.logout()

d138fc149bfbfe0d65ff64101633c267489fad0f
Anastasiya S <nastya@status.im>
2023-02-21 12:04:39 +0300

    review addressed: extracted methods for filling in forms (group and contact)

--- fixture/contact.py
+++ fixture/contact.py
@@ -23,16 +23,20 @@ class ContactHelper:
     def add_contact_without_group(self, contact):
         wd = self.app.wd
         self.init_contact_creation_home_page()
-        # fill in contact form (group value = None), allowed only for contact creation
+        self.fill_in_contact_form_without_group(contact)
+        # select None value for the group
+        wd.find_element_by_xpath("//option[@value='[none]']").click()
+        self.submit_contact_form_creation()
+        self.return_to_home_page_from_confirmation()
+
+    def fill_in_contact_form_without_group(self, contact):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.contact_first_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys(contact.contact_last_name)
-        wd.find_element_by_xpath("//option[@value='[none]']").click()
-        self.submit_contact_form_creation()
-        self.return_to_home_page_from_confirmation()
 
     def submit_contact_form_creation(self):
         wd = self.app.wd
@@ -51,16 +55,9 @@ class ContactHelper:
         self.open_home_page_in_header()
         # TODO: might be helpful in future when specifying exact contact to edit
         # wd.find_element_by_name("selected[]").click()
-        # click pencil icon for random contact actually, because this icon is the same for all contacts
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # fill in contact form for modification (no group field)
-        # TODO: contact edit form does not have [group] field
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.contact_first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.contact_last_name)
+        self.fill_in_contact_form_without_group(contact)
         # submit contact form modification (click Update)
         wd.find_element_by_xpath("//input[@value='Update']").click()
         self.return_to_home_page_from_confirmation()
--- fixture/group.py
+++ fixture/group.py
@@ -19,6 +19,13 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill in group form
+        self.fill_in_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page_from_confirmation()
+
+    def fill_in_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.group_name)
@@ -28,9 +35,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.group_footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page_from_confirmation()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -49,15 +53,7 @@ class GroupHelper:
         # initiate group modification
         wd.find_element_by_xpath("//input[@value='Edit group']").click()
         # fill in group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.group_name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.group_header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+        self.fill_in_group_form(group)
         # submit the form for changed group (click Update)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page_from_confirmation()

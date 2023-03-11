f781bf2bc80043c5b4037fd412c8d71777427597
Anastasiya S <nastya@status.im>
2023-03-11 19:20:21 +0300

    added random test data generator for group creation test

--- model/group.py
+++ model/group.py
@@ -10,12 +10,19 @@ class Group:
         self.group_id = group_id
 
     def __repr__(self):
-        return "%s:%s" % (self.group_id, self.group_name)
+        return "%s:%s:%s:%s" % (self.group_id, self.group_name, self.group_header, self.group_footer)
 
     def __eq__(self, other):
-        return (self.group_id is None or other.group_id is None
-                or self.group_id == other.group_id) and (
-                self.group_name is None or other.group_name is None or self.group_name == other.group_name)
+        return (
+                self.group_id is None or other.group_id is None
+                or self.group_id == other.group_id) \
+            and (
+                self.group_name is None or other.group_name is None or self.group_name == other.group_name) \
+            and (
+                self.group_header is None or other.group_header is None or self.group_header == other.group_header) \
+            and (
+                self.group_footer is None or other.group_footer is None or self.group_footer == other.group_footer
+        )
 
     def id_or_max(self):
         if self.group_id:
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
+import pytest
+import random
+import string
 
 from model.group import Group
 
 
-def test_add_group(app):
-    groups_list_before = app.group.get_group_list()
-    group_to_add = Group(
-        group_name="test group",
-        group_header="test group header",
-        group_footer="test group footer")
-    app.group.create(group_to_add)
-    assert len(groups_list_before) + 1 == app.group.count()
-    groups_list_after = app.group.get_group_list()
-    groups_list_before.append(group_to_add)
-    assert sorted(groups_list_before, key=Group.id_or_max) == sorted(groups_list_after, key=Group.id_or_max)
+def random_string(prefix, maxlen):
+    symbols_to_use = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols_to_use) for i in range(random.randrange(maxlen))])
 
 
-def test_add_empty_group(app):
+test_data = [Group(group_name="", group_header="", group_footer="")] + \
+            [
+            Group(
+                group_name=random_string("group name", 10),
+                group_header=random_string("group header", 20),
+                group_footer=random_string("group footer", 20))
+            for i in range(5)
+]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     groups_list_before = app.group.get_group_list()
-    group_to_add = Group(
-        group_name="test group",
-        group_header="test group header",
-        group_footer="test group footer")
+    group_to_add = group
     app.group.create(group_to_add)
     assert len(groups_list_before) + 1 == app.group.count()
     groups_list_after = app.group.get_group_list()
